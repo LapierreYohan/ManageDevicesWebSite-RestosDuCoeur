1160bec7d00fd0ade1509aac86b2d592a2eba413
Yohan <77733985+Galtrips@users.noreply.github.com>
2022-10-16 15:23:22 +0200

    📦 NEW: Connexion page

--- index.php
+++ index.php
@@ -5,10 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Accueil</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <title>Connexion</title>
+
+    <link rel="stylesheet" href="./css/connexion.css">
 </head>
 <body>
-    
+    <img class="login" src="./img/login-background.png" alt="Enseigne des Restos du coeur">
+    <form action="">
+
+        <input type="text" name="Identifiant" id="identifiant" class="login" placeholder="Compte utilisateur">
+        <input type="password" name="Mdp" id="mdp" class="mdp" placeholder="Mot de passe">
+        <button type="submit" class="loginSubmit">S'indentifier</button>
+        <hr>
+        <a href="">Vous avez oublié votre mot de passe ?</a>
+
+    </form>
 </body>
 </html>
