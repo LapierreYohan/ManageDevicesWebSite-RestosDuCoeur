b7cdfade5aae958a098a1b4bdaede35505815d59
Yohan <77733985+Galtrips@users.noreply.github.com>
2022-10-17 19:05:01 +0200

    📦 NEW:  Update with a local host connexion

--- include/MariaDB.php
+++ include/MariaDB.php
@@ -1,16 +1,31 @@
 <?php
-const db = "p2103678";
-const dbhost = "iutbg-lamp.univ-lyon1.fr";
+
+// const db = "p2103678";
+// const dbhost = "iutbg-lamp.univ-lyon1.fr";
+// const dbport = 3306;
+// const dbuser = "p2103678";
+// const dbpasswd = "12103678";
+
+const db = "resto";
+const dbhost = "localhost";
 const dbport = 3306;
 const dbuser = "p2103678";
 const dbpasswd = "12103678";
 
-function bddRestos(): PDO
-{
+function bddRestos(): PDO {
     $bdd = new PDO('mysql:host=' . dbhost . ';port=' . dbport . ';dbname=' . db . '', dbuser, dbpasswd);
     $bdd->exec("SET CHARACTER SET utf8");
     return $bdd;
 }
+
+// $stmt = $bdd->prepare("SELECT * FROM Utilisateur");
+// $stmt->execute();
+
+// $res = $stmt->fetchAll();
+// foreach ( $res as $row ) {
+//     print_r($row);
+// }
+
 ?>
 
 
