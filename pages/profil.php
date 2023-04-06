929eda1e33837c8b45fcab5ca5baa34059cd6957
Galtrips <galtrips.pvp@gmail.com>
2023-04-06 08:48:45 +0200

    Blbl

--- pages/profil.php
+++ pages/profil.php
@@ -49,6 +49,7 @@ if (!empty($_GET["id"])) {
 
 ?>
 
+
 <html lang="fr">
 
 <head>
@@ -59,86 +60,200 @@ if (!empty($_GET["id"])) {
 </head>
 
 <body>
-    <?php require_once __DIR__ . "/../modules/header.php"; ?>
-    <section class="container">
-        <div class="bg bg-secondary rounded-circle" style="width: 350px; height: 350px;">
-            <img src="<?php if ($_idurl == true) {
-                            echo $rec[0]['Image'];
-                        } else {
-                            echo $_SESSION['User']['Image'];
-                        } ?>" style=" margin-left: 10px; margin-top: 10px; margin-bottom: 40px;" alt="" width="330" height="330" class="rounded-circle me-2">
-        </div>
-        <div style="margin-top: 25px; margin-left: 25px;">
-            <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-            Changer de photo de profile : <input type="file" name="MaPhoto  " />
-        </div>
-        <div class="border border-secondary border-3 rounded-4 bg bg-light" style="margin: 40px 2% 30px 2%;">
-            <form method="POST" action="/includes/traitements/modifyUser.php" style="margin: 30px 2% 0 2%;">
-                <h4 class="mb-3">Informations personnelles</h4>
-                <div class="mb-4">
-                    <div class="input-group">
-                        <label class="input-group-text">Prenom</label>
-                        <input type="text" class="form-control" id="prenom" name="prenom" value="<?php if ($_idurl) {
+<?php require_once __DIR__ . "/../modules/header.php"; ?>
+<div class="container">
+    <div class="main-body">
+    
+          <!-- Breadcrumb 
+          <nav aria-label="breadcrumb" class="main-breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
+              <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+            </ol>
+          </nav>
+           /Breadcrumb -->
+    
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3" style="margin-top: 20px;">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4 class="text-light"><?php if ($_idurl) {
+                                                                  echo $rec[0]['Prenom'];
+                                                                 } else {
+                                                                  echo $_SESSION["User"]["Prenom"];
+                                                                } ?></h4>
+                      <!-- <p class="text-light mb-1">
+                        <?php if ($rec[0]['admin']) {
+                            echo "Administrateur";
+                        } else {                                // à remttre plus tard avec la nouvelle bdd
+                            echo "Non administrateur";
+                        } ?>
+                      </p> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--
+              <div class="card mt-3">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
+                    <span class="text-light">https://bootdey.com</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
+                    <span class="text-light">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
+                    <span class="text-light">@bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
+                    <span class="text-light">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+                    <span class="text-light">bootdey</span>
+                  </li>
+                </ul>
+              </div>
+              !-->
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0 text-light">Full Name</h6>
+                    </div>
+                    <div class="col-sm-9 text-light">
+                    <input type="text" class="form-control bg-dark text-light" id="prenom" name="prenom" value="<?php if ($_idurl) {
                                                                                                         echo $rec[0]['Prenom'];
                                                                                                     } else {
                                                                                                         echo $_SESSION["User"]["Prenom"];
                                                                                                     } ?>" maxlength="47" required>
                     </div>
-                </div>
-                <div class="mb-4">
-                    <div class="input-group">
-                        <label class="input-group-text">Nom</label>
-                        <input type="text" class="form-control" id="nom" name="nom" value="<?php if ($_idurl) {
-                                                                                                echo $rec[0]['Nom'];
-                                                                                            } else {
-                                                                                                echo $_SESSION["User"]["Nom"];
-                                                                                            } ?>" maxlength="47" required>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0 text-light">Email</h6>
                     </div>
-                </div>
-                <div class="mb-4">
-                    <div class="input-group">
-                        <p class="input-group-text">Reférence</p>
-                        <p class="form-control"><?php if ($_idurl) {
-                                                    echo $rec[0]['Reference_User'];
-                                                } else {
-                                                    echo $_SESSION["User"]["Reference_User"];
-                                                } ?></p>
-                    </div>
-                    <input type="hidden" id="id" name="id" value="<?php if (!empty($_GET["id"])) {
-                                                                        echo $_GET["id"];
-                                                                    } else {
-                                                                        echo -1;
-                                                                    } ?>">
-                </div>
-                <div class="mb-4">
-                    <div class="input-group">
-                        <p class="input-group-text">Mail</p>
-                        <p class="form-control"><?php if ($_idurl) {
+                    <div class="col-sm-9 text-light">
+                      <input type="text" class="form-control bg-dark text-light" id="prenom" name="prenom" value="<?php if ($_idurl) {
                                                     echo $rec[0]['Mail'];
                                                 } else {
                                                     echo $_SESSION["User"]["Mail"];
-                                                } ?></p>
+                                                } ?>" maxlength="47" required>
                     </div>
-                </div>
-                <div class="mb-4">
-                    <div class="input-group">
-                        <p class="input-group-text">Créateur</p>
-                        <p class="form-control"><?= $res ?></p>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0 text-light">Phone</h6>
                     </div>
-                </div>
-                <div class="col mb-4">
-                    <div class="input-group">
-                        <label class="input-group-text">Commentaire</label>
-                        <input type="text" class="form-control" id="com" name="com" value="<?php if ($_idurl) {
-                                                                                                echo $rec[0]['Commentaire'];
-                                                                                            } else {
-                                                                                                echo $_SESSION["User"]["Commentaire"];
-                                                                                            } ?>" maxlength="47" required>
+                    <div class="col-sm-9 text-light">
+                      (239) 816-9029
                     </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0 text-light">Mobile</h6>
+                    </div>
+                    <div class="col-sm-9 text-light">
+                      (320) 380-4539
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0 text-light">Address</h6>
+                    </div>
+                    <div class="col-sm-9 text-light">
+                      Bay Area, San Francisco, CA
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <a class="btn btn-outline" style="background-color:#d60ea1; border: none;    " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
+                    </div>
+                  </div>
                 </div>
-                <button class="w-35 btn btn-outline-secondary btn-lg mb-4" type="submit" style=" margin-left: 2%;">Enregistrer</button>
-            </form>
+              </div>
+                <!--
+              <div class="row gutters-sm">
+                <div class="col-sm-6 mb-3">
+                  <div class="card h-100">
+                    <div class="card-body">
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">assignment</i>Project Status</h6>
+                      <small>Web Design</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Website Markup</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>One Page</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Mobile Template</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Backend API</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6 mb-3">
+                  <div class="card h-100">
+                    <div class="card-body">
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">assignment</i>Project Status</h6>
+                      <small>Web Design</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Website Markup</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>One Page</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Mobile Template</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <small>Backend API</small>
+                      <div class="progress mb-3" style="height: 5px">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              !-->
+
+
+
+            </div>
+          </div>
+
         </div>
+    </div>
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
